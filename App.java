081e9a3362f950d6c9291b554867a7a2b63277b8
Jonathan Bobko <jonbobko@outlook.com>
2024-02-15 11:08:37 -0700

    fourth commit

--- App.java
+++ App.java
@@ -11,6 +11,46 @@ public class App {
 		
 		Deck deck = new Deck();
 		deck.describe();
+		
+		Player player = new Player();
+		Player player2 = new Player();
+		
+		deck.shuffle();
+		
+		for(int i = 0; i < 52; i++) {
+			if (i % 2 == 0) {
+				player.draw();
+			}
+			else {
+				player2.draw();
+			}
+		}
+		for (int i = 0; i < 26; i++) {
+		(player.flip()).describe();
+		player2.flip().describe();
+		if (player.value > player2.value) {
+			player.incrementScore();
+			System.out.println("Player1 recieved a point");
+		}else if(player.value < player2.value) {
+			player2.incrementScore();
+			System.out.println("Player2 recieved a point");
+		}else if(player.value == player2.value) {
+			System.out.println("No point was awarded");
+		}
+		}
+		
+		if (player.score > player2.score) {
+			System.out.println("Player 1 Score: " + player.score + " " + "Player 2 Score: " + player2.score + 
+					"\nPlayer 1 is the winner!");
+		}else if(player.score < player2.score) {
+			System.out.println("Player 1 Score: " + player.score + " " + "Player 2 Score: " + player2.score + 
+					"\nPlayer 2 is the winner!");
+		}else {
+			System.out.println("Player 1 Score: " + player.score + " " + "Player 2 Score: " + player2.score + 
+					"\nIt's a tie!");
+		}
+		
+		
 	}
 
 }
--- Player.java
+++ Player.java
@@ -1,5 +1,48 @@
 package Week06Project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+	int score;
+	
+	public Player() {
+		int score = 0;
+		
+		// TODO Auto-generated constructor stub
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	List<Card> hand = new ArrayList<>(); 
+	
+	
+	
+	
+	
+
+
+public void describeHand () {
+	for (Card : hand) {
+		card.describe();
+	}
+	}
+	
+public List<Card> flip(hand) {
+	return hand.get(0);
+	
+}
+
+public List<Card> draw(hand) {
+	return hand.draw();
+}
+public int incrementScore () {
+	return score += 1;
+}
+
+	
 
 }
